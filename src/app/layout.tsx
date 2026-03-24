72528926273f8da0f2c7e15b6ab74283a867cc94
snehygupta <snehy.gupta@techdome.net.in>
2026-03-24 05:35:44 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,16 +17,6 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>{children}</body>
-      <footer className='flex items-center justify-center w-full h-24 border-t text-emerald-700 font-semibold'>
-        <a
-          className='flex items-center justify-center'
-          href='https://artifactbin.com?utm_source=template-repo&utm_campaign=oss'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Deployed by Artifact Bin
-        </a>
-      </footer>
     </html>
   );
 }
